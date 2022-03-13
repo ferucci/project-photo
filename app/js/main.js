37f41f7548f6ddda01fe21b3a734b582cb1270f0
ferucci <development.first1@gmail.com>
2022-03-14 01:10:47 +0300

    add script

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,16 @@ menuButton.addEventListener('click', () => {
     menuLink.classList.toggle('aside-active');
     menuButton.classList.toggle('menu-button--active');
 });
+
+let links = document.getElementsByClassName("menu__link");
+let activeLink = document.getElementsByClassName('menu__link--active');
+let i;
+for (i = 0; links.length > i; i++){
+    links[i].onclick = function(){
+        let currentActive = activeLink[0];
+        if (currentActive)
+            currentActive.classList.remove("menu__link--active");
+        if (currentActive !== this)
+            this.classList.add("menu__link--active");    
+    };
+}
