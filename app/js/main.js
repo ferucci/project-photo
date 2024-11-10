bb64e9997e45ae55d7f610063a19fd121b23d6d7
Ferucci <development.first1@gmail.com>
2024-11-10 14:54:41 +0300

    rewrote scss to the new style, removed imports, used "use"

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,61 @@
-'use strict'
+class MenuController {
+    constructor() {
+        this.menuButton = document.querySelector('.header__menu-button');
+        this.asideBlock = document.querySelector('.aside');
+        this.sections = document.querySelectorAll('.content');
+        this.links = document.getElementsByClassName("menu__link");
 
-let menuButton = document.querySelector('.header__menu-button');
-let menuLink = document.querySelector('.aside');
+        this.init();
+    }
 
-menuButton.addEventListener('click', () => {
-    menuLink.classList.toggle('aside-active');
-    menuButton.classList.toggle('menu-button--active');
-});
+    init() {
+        this.menuButton.addEventListener('click', this.toggleAsideClasses.bind(this));
 
-let links = document.getElementsByClassName("menu__link");
-let activeLink = document.getElementsByClassName('menu__link--active');
-let i;
-for (i = 0; links.length > i; i++){
-    links[i].onclick = function(){
-        let currentActive = activeLink[0];
-        if (currentActive)
-            currentActive.classList.remove("menu__link--active");
-        if (currentActive !== this)
-            this.classList.add("menu__link--active");    
-    };
+        Array.from(this.links).forEach(link => {
+            link.addEventListener('click', this.handleLinkClick.bind(this, link));
+        });
+
+        this.activateInitialLink();
+    }
+
+    activateInitialLink() {
+        const initialActiveLink = Array.from(this.links).find(link => link.classList.contains('menu__link--active'));
+        if (initialActiveLink) {
+            this.changeContent(initialActiveLink);
+        }
+    }
+
+    handleLinkClick(link) {
+        this.deactivateCurrentActiveLink();
+        link.classList.add('menu__link--active');
+        this.changeContent(link);
+    }
+
+    deactivateCurrentActiveLink() {
+        Array.from(this.links).forEach(link => {
+            link.classList.remove('menu__link--active');
+        });
+    }
+
+    toggleAsideClasses() {
+        this.asideBlock.classList.toggle('aside-active');
+        this.menuButton.classList.toggle('menu-button--active');
+    }
+
+    changeContent(link) {
+        const contentId = link.getAttribute('href').replace('#', '');
+
+        if (this.asideBlock.classList.contains('aside-active')) {
+            this.toggleAsideClasses();
+        }
+
+        this.sections.forEach(section => {
+            section.style.display = 'none';
+            if (section.hasAttribute('data-content') && section.dataset.content === contentId) {
+                section.style.display = 'block';
+            }
+        });
+    }
 }
+
+document.addEventListener('DOMContentLoaded', () => new MenuController());
